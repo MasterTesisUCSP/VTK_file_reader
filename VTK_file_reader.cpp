31e7a3fadad6efb042586903bdc194b8c60a52d2
Fil <fvilca@gmail.com>
2018-10-20 17:21:38 -0500

    files

---
+++ VTK_file_reader.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include <string>
+//#include "DicomReader.h"
+using namespace std;
+#include "ReadUnstructuredGrid2.h"
+
+
+int main(int argc, char* argv[]) {
+
+
+	//DicomReader();
+	string filename = argv[1];
+	execute(filename);
+	
+	return 1;
+
+}
